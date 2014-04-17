9abf28c5d83339c696709859bcdb9b55f51a279d
Arun Hariharan <arun_nh@yahoo.com>
2014-04-17 14:12:53 -0700

    Fixed XML annotations

--- UMapUSUI/src/main/java/com/mysoft/web/controllers/AfterLoginController.java
+++ UMapUSUI/src/main/java/com/mysoft/web/controllers/AfterLoginController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 
 
@@ -16,7 +15,6 @@ public class AfterLoginController {
 	public String afterLogin(ModelMap model)
 	{
 		model.addAttribute("message","Shinku Tatsumaki");
-		
 		return "UMapUSWork";
 	}
 	
