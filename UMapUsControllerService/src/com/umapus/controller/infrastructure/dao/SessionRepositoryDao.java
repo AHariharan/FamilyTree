bc8d35d6482c76d7d76aa1117c757044a1906f4a
vglvishnu <vglvishnu@gmail.com>
2014-06-30 09:58:26 -0700

    Refactoring and new class

---
+++ UMapUsControllerService/src/com/umapus/controller/infrastructure/dao/SessionRepositoryDao.java
@@ -0,0 +1,10 @@
+package com.umapus.controller.infrastructure.dao;
+
+import java.util.HashMap;
+
+public interface SessionRepositoryDao {
+	public void AddToRedis(String key, HashMap<String,String> hashvalue);
+	public HashMap<String,String> ReadFromRedis(String key);
+	public void UpdateToRedis(String key, HashMap<String,String> hashValue);
+	public void DeleteFromRedis(String key);
+}
