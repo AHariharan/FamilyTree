116fe13f377514f6d5c16ec5a3b9c57c72762442
vglvishnu <vglvishnu@gmail.com>
2014-07-30 09:23:57 -0700

    Add init-param to point to the service pkg. Now that I realize I might have to convert this project to a pojo

--- UMapUsControllerService/src/com/umapus/controller/service/UMapUsControllerService.java
+++ UMapUsControllerService/src/com/umapus/controller/service/UMapUsControllerService.java
@@ -1,11 +1,27 @@
 package com.umapus.controller.service;
 
-import javax.ws.rs.Path;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
 import org.springframework.stereotype.Controller;
+
 
 @Controller
 @Path("umapuscontrollerservice")
 public class UMapUsControllerService {
 
+	@POST
+	@Path("/login")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response Login(String jsonBody, String req)
+			throws JSONException {
+      return null;
+	}
+	
 }
