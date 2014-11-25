ded07ad9d1b4a97c235552ee019bbd67a365d166
AHariharan <arun_nh@yahoo.com>
2014-11-24 23:37:31 -0800

    Fixed the path

--- UMapUsControllerService/src/com/umapus/controller/domain/util/EmailHelper.java
+++ UMapUsControllerService/src/com/umapus/controller/domain/util/EmailHelper.java
@@ -1,8 +1,13 @@
 package com.umapus.controller.domain.util;
 
+import java.io.StringWriter;
 import java.util.HashMap;
+import java.util.Set;
 
+import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.velocity.VelocityEngineUtils;
 
@@ -13,15 +18,31 @@ public class EmailHelper {
 	@Autowired
 	public EmailHelper(VelocityEngine velocityEngine){
 		this.velocityEngine = velocityEngine;	
+		velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+		velocityEngine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+		velocityEngine.init();
 	}
 	
 	public String CreateSignUpEmail(HashMap<String,Object> varMap){
 		
-		String body = VelocityEngineUtils.
+		
+		System.out.println("Velocity Resource path " + velocityEngine.getTemplate("SignUpEmail.vm"));
+		
+		VelocityContext context = new VelocityContext();
+		for(String key : varMap.keySet())
+		{
+			context.put(key, varMap.get(key));
+		}
+	
+		StringWriter writer = new StringWriter();
+		
+		velocityEngine.mergeTemplate("SignUpEmail.vm", "UTF-8", context, writer);
+		
+		/*String body = VelocityEngineUtils.
 				mergeTemplateIntoString
 				(velocityEngine, 
-				 "/SignUpEmail.vm","UTF-8",varMap);
+				 "/SignUpEmail.vm","UTF-8",varMap);*/
 		
-		return body;
+		return writer.toString();
 	}
 }
